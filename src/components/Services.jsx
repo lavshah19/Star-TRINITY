97432330e2ac23d08841b05aa13363de7575c00d
lavshah19 <lavshah51@gmail.com>
2024-11-17 08:06:43 -0600

    changes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -22,7 +22,7 @@ const Services = () => {
         "Electrician",
         "Pipe Fitter",
         "Plumber",
-        "Rigger (Certified)"
+        
       ],
       img: "https://images.unsplash.com/photo-1621905251918-48416bd8575a?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
@@ -31,8 +31,7 @@ const Services = () => {
       items: [
         "Supervisor",
         "Time Keeper",
-        "Manlift Operator",
-        "Forklift Operator",
+        
       ],
       img: "https://plus.unsplash.com/premium_photo-1661962226921-4ae283c4be59?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
@@ -42,8 +41,6 @@ const Services = () => {
         "Fabricator (Certified)",
         "Painter",
         "Insulator",
-        "Flagman / Banksman",
-        "Welder (Certified)",
         "Duct Man",
       ],
       img: "https://images.unsplash.com/photo-1513350375601-5040cae53a97?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
